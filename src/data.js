67b5b259299e560faf8ad81edfdbc8dc6f823a02
shirley <shirleymalefane0019@gmail.com>
2024-02-16 23:04:20 +0900

    emojis

--- src/data.js
+++ src/data.js
@@ -2,66 +2,78 @@ const data = [
     {
     "id": 1,
     "title": "LOML",
-    "description": "Love of my life "
+    "description": "Love of my life ",
+    "image":"https://pbs.twimg.com/media/EkT6c9jXgAEaiuV.jpg"
     },
     {
     "id": 2,
     "title": "ROFL ",
-    "description": "Rolling on the floor laughing"
+    "description": "Rolling on the floor laughing",
+    "image":"https://cdn.emojidex.com/emoji/seal/Rofl-intext.png?1591641894"
     },
     {
     "id": 3,
     "title": "OMG",
-    "description": "Oh my gosh"
+    "description": "Oh my gosh",
+    "image":"https://m.media-amazon.com/images/I/51JZfKjQDDL._AC_UF894,1000_QL80_.jpg"
     },
     {
     "id": 4,
     "title": "BRB",
-    "description": "Be right back"
+    "description": "Be right back",
+    "image":"https://i.pinimg.com/474x/06/4f/8d/064f8d0a29592d66ba9fdeedb2a053d9.jpg"
     },
 
     {
         "id": 5,
         "title": "IKR ",
-        "description": "I know right!"
+        "description": "I know right!",
+        "image":"https://4.bp.blogspot.com/-kdoKwz91qag/WLsewK2oxpI/AAAAAAAAU_k/dphMQlen2lYZWMCXqmG-UR0DsrL6zbVEgCLcB/s1600/shrug-smiley.png"
         },
         {
         "id": 6,
         "title": "TBH",
-        "description": "To be honest"
+        "description": "To be honest",
+        "image":"https://images.thequint.com/thequint%2F2019-07%2F24a64aab-5c73-4837-9e41-fb40036e9876%2Fneta_ji.jpg"
         },
         {
         "id": 7,
         "title": "BTW",
-        "description": "By the way"
+        "description": "By the way",
+        "image":"https://i.pinimg.com/474x/06/95/9d/06959dbbe2f931cf97b286784388e161.jpg"
         },
 
             {
     "id": 8,
     "title": "TTYL",
-    "description": "Talk to you later"
+    "description": "Talk to you later",
+    "image":"https://janicecarmell.files.wordpress.com/2014/04/emoji-thumb.jpg"
     },
     {
     "id": 9,
     "title": "KMT",
-    "description": "Kiss my teech: Mtcheew!"
+    "description": "Kiss my teech: Mtcheew!",
+    "image":"https://pbs.twimg.com/media/DVwpcCIXkAE-7rx.jpg"
     },
     {
     "id": 10,
-    "title": "BRB",
-    "description": "Be right back"
+    "title": "BTW",
+    "description": "By the way",
+    "image":"https://ih1.redbubble.net/image.334373800.5231/fposter,small,wall_texture,product,750x1000.u3.jpg"
     },
 
     {
     "id": 11,
     "title": "IYKYK",
-    "description": "If you know you know"
+    "description": "If you know you know",
+    "image":"https://clipart-library.com/newimages/free-emoji-clipart-7.jpg"
     },
 
     {
      "id": 12,
      "title": "OYO",
-     "description": "On your own"
+     "description": "On your own",
+     "image":"https://i.pinimg.com/736x/bc/2d/23/bc2d231e3ec30f87275950c690194c4e.jpg"
     }
     
 ]
